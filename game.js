ab22577273d42c87bb169c8139a1e47633a7cff6
Keanem <mercadokeane32@gmail.com>
2021-02-22 07:42:50 +1300

    complete challenge again

---
+++ game.js
@@ -0,0 +1,64 @@
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
+// https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  bindEventListeners(document.getElementsByClassName("board")[0].children);
+}
+
+function bindEventListeners(dots) {
+  for (var i = 0; i < dots.length; i++) {
+    // BIND YOUR EVENT LISTENERS HERE
+    // The first one is provided for you
+    dots[i].addEventListener("contextmenu", makeGreen);
+    dots[i].addEventListener("click", makeBlue);
+    dots[i].addEventListener("dblclick", hide);
+  }
+}
+
+function makeGreen(evt) {
+  evt.preventDefault();
+  evt.target.classList.toggle("green");
+  updateCounts();
+}
+
+// CREATE FUNCTION makeBlue HERE
+
+function makeBlue(evt) {
+  evt.target.classList.toggle("blue");
+  updateCounts();
+}
+
+// CREATE FUNCTION hide HERE
+
+function hide(evt) {
+  evt.target.classList.toggle("invisible");
+  updateCounts();
+}
+
+function updateCounts() {
+  var totals = {
+    blue: counting("blue"),
+    green: counting("green"),
+    invisible: counting("invisible")
+  };
+
+  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+
+  function counting(string) {
+    let theCount = document.getElementsByClassName(string);
+    return theCount.length;
+  }
+
+  counting();
+
+  // Once you've done the counting, this function will update the display
+  displayTotals(totals);
+}
+
+function displayTotals(totals) {
+  for (var key in totals) {
+    document.getElementById(key + "-total").innerHTML = totals[key];
+  }
+}
